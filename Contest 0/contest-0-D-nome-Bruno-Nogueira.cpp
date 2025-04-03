940020385003f4fdb77c5fce8c42065e0a60c347
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2025-04-03 18:12:29 +0000

    Problem D Continue

---
+++ Contest 0/contest-0-D-nome-Bruno-Nogueira.cpp
@@ -0,0 +1,23 @@
+// https://vjudge.net/contest/706892#problem/D
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define R sqrt(1'000'000'000)
+
+int main(int argc, char const *argv[])
+{
+
+    vector<long long> primes(R, false);
+    primes[2] = true;
+    for (long long i = 3; i < R; i++)
+    {
+        
+    }
+
+    int t;
+    cin >> t;
+
+    return 0;
+}
