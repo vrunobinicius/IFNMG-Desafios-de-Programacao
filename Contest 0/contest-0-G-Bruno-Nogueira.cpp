fc6cc7374fb22cdc5f8b004417c2ae456d970250
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2025-04-04 23:17:12 -0300

    Problem G

---
+++ Contest 0/contest-0-G-Bruno-Nogueira.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n--)
+    {
+        string word;
+        cin >> word;
+
+        int endOfWord = word.size() - 1;
+
+        if (word.size() > 10)
+            cout << word[0] << word.size() - 2 << word[endOfWord] << "\n";
+        else
+            cout << word << "\n";
+    }
+
+    return 0;
+}
