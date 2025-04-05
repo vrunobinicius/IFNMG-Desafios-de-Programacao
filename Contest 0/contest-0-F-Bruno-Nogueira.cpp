07920f88310f2e2e3e7f4240b2f6ff6bb41d348c
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2025-04-04 23:03:39 -0300

    Problem F

---
+++ Contest 0/contest-0-F-Bruno-Nogueira.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int l1, l2, l3;
+        cin >> l1 >> l2 >> l3;
+
+        if ((l1 == l2 && l3 % 2 == 0) ||
+            (l1 == l3 && l2 % 2 == 0) ||
+            (l2 == l3 && l1 % 2 == 0) ||
+            (l1 + l2 == l3) ||
+            (l1 + l3 == l2) ||
+            (l2 + l3 == l1))
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
