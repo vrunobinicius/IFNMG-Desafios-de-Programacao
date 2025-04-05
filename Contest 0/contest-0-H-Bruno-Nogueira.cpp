ad4dd571a97cabc80b4b90906a45e7c3277a6015
Bruno Vinícius Alves Nogueira <100003070+vrunobinicius@users.noreply.github.com>
2025-04-04 23:46:29 -0300

    Problem H

---
+++ Contest 0/contest-0-H-Bruno-Nogueira.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n, k, x;
+
+    cin >> n >> k >> x;
+
+    vector<int> chores(n);
+    for (int i = 0; i < n; i++)
+        cin >> chores[i];
+
+    int lastChores = chores.size() - 1;
+    while (k--)
+    {
+        chores[lastChores] = x;
+        lastChores--;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < chores.size(); i++)
+        sum += chores[i];
+
+    cout << sum << "\n";
+
+    return 0;
+}
